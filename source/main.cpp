3a0fbe2e0e74e4bc74c535f310558ecac8a94871
Aaron Jacobs <aaron@aaronmjacobs.com>
2015-09-06 12:57:26 -0700

    Implement controller tester

--- source/main.cpp
+++ source/main.cpp
@@ -19,6 +19,89 @@ public:
 };
 
 void TestEngine::tick(const float dt) {
+   SPtr<const Controller> controller(getController(0));
+   if (!controller) {
+      return;
+   }
+
+   const ControllerValues &values = controller->values();
+
+   // Axes
+   if (values.leftThumbHorizontal != 0.0f) {
+      LOG_DEBUG("Left thumb horizontal: " << values.leftThumbHorizontal);
+   }
+   if (values.leftThumbVertical != 0.0f) {
+      LOG_DEBUG("Left thumb vertical: " << values.leftThumbVertical);
+   }
+
+   if (values.rightThumbHorizontal != 0.0f) {
+      LOG_DEBUG("Right thumb horizontal: " << values.rightThumbHorizontal);
+   }
+   if (values.rightThumbVertical != 0.0f) {
+      LOG_DEBUG("Right thumb vertical: " << values.rightThumbVertical);
+   }
+
+   if (values.leftTriggerAxis != -1.0f) {
+      LOG_DEBUG("Left trigger axis: " << values.leftTriggerAxis);
+   }
+   if (values.rightTriggerAxis != -1.0f) {
+      LOG_DEBUG("Right trigger axis: " << values.rightTriggerAxis);
+   }
+
+   // Buttons
+   if (values.dPadLeft) {
+      LOG_DEBUG("D-pad left");
+   }
+   if (values.dPadRight) {
+      LOG_DEBUG("D-pad right");
+   }
+   if (values.dPadUp) {
+      LOG_DEBUG("D-pad up");
+   }
+   if (values.dPadDown) {
+      LOG_DEBUG("D-pad down");
+   }
+
+   if (values.faceLeft) {
+      LOG_DEBUG("Left face");
+   }
+   if (values.faceRight) {
+      LOG_DEBUG("Right face");
+   }
+   if (values.faceUp) {
+      LOG_DEBUG("Up face");
+   }
+   if (values.faceDown) {
+      LOG_DEBUG("Down face");
+   }
+
+   if (values.leftTrigger) {
+      LOG_DEBUG("Left trigger");
+   }
+   if (values.rightTrigger) {
+      LOG_DEBUG("Right trigger");
+   }
+
+   if (values.leftBumper) {
+      LOG_DEBUG("Left bumper");
+   }
+   if (values.rightBumper) {
+      LOG_DEBUG("Right bumper");
+   }
+
+   if (values.leftThumb) {
+      LOG_DEBUG("Left thumb");
+   }
+   if (values.rightThumb) {
+      LOG_DEBUG("Right thumb");
+   }
+
+   if (values.leftMenu) {
+      LOG_DEBUG("Left menu");
+   }
+   if (values.rightMenu) {
+      LOG_DEBUG("Right menu");
+   }
 }
 
 void TestEngine::render() {
